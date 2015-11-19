93838d20fc8b08b460dd31c3f88506c0c6e17c3e
whiskysoda <ryaninputney@yahoo.co.uk>
2015-11-19 09:24:36 +0000

    Added in account.is_staff=true

--- thinkster/authentication/models.py
+++ thinkster/authentication/models.py
@@ -25,6 +25,7 @@ class AccountManager(BaseUserManager):
         account = self.create_user(email, password, **kwargs)
 
         account.is_admin=True
+        account.is_staff=True
         account.save()
         
         return account
